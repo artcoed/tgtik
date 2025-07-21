848ae7f26b547094948dbe1521dd1b35e7c3781a
Artyom <artbar.ru@gmail.com>
2025-07-21 13:08:52 +0500

    Refactoring

--- client/src/components/VideoProgressBar.tsx
+++ client/src/components/VideoProgressBar.tsx
@@ -3,15 +3,17 @@ import styles from './VideoProgressBar.module.css';
 
 interface VideoProgressBarProps {
   progress: number;
+  transition?: boolean;
 }
 
-function VideoProgressBar({ progress }: VideoProgressBarProps) {
+function VideoProgressBar({ progress, transition = true }: VideoProgressBarProps) {
+  const transitionStyle = transition ? 'width 0.3s cubic-bezier(.4,0,.2,1)' : 'none';
   return (
     <div className={styles.videoProgressBar}>
         <div className={styles.videoProgressBarContainer}>
-            <div className={styles.videoProgressPlayed} style={{width: `${progress * 100}%`, transition: "1000ms"}} />
-            <div className={styles.videoProgressUnplayed} style={{left: `${progress * 100}%`, transition: "1000ms", width: `${(1 - progress) * 100}%`}} />
-            <div className={styles.videoProgressThumb} style={{left: `calc(${progress * 100}%)`, transition: "1000ms"}} />
+            <div className={styles.videoProgressPlayed} style={{width: `${progress * 100}%`, transition: transitionStyle}} />
+            <div className={styles.videoProgressUnplayed} style={{left: `${progress * 100}%`, transition: transitionStyle, width: `${(1 - progress) * 100}%`}} />
+            <div className={styles.videoProgressThumb} style={{left: `calc(${progress * 100}%)`, transition: transitionStyle}} />
         </div>
     </div>
   );
